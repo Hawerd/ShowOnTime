50444ee43f285406eea249d6b57de9be8874f966
Luis F Casta¤o <cast9104@gmail.com>
2016-05-22 16:30:19 -0500

    LFC Se ajustan los archivos Portal-V, Sidebar y Tabs.xml, este ultimo archivo se renombra por Tabbar.xml, se anexan historial de comentarios  a algunos archivos.

--- portal/Portal-V.php
+++ portal/Portal-V.php
@@ -1,73 +1,93 @@
 <!DOCTYPE html>
 <!--
-  
     Name:   Portal-V.php
     Autor:  Hawerd Gonzalez
-    Desc:   Archivo inicial que contiene los taps del programa
+    Date:   22-May-2016
+    Desc:   Modulo principal del Portal.
+
+    Autor:  Luis castaño
+    Date:   22-May-2016  
+    Desc:   Se ajusta  el codigo del portal, se agrega la seccion 
+            INICIALIZACION.
     
 -->
 <html>
 <head>
-    <title></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
-    <link href="../codebase/skyblue/dhtmlx.css" rel="stylesheet" type="text/css"/>
-    <script src="../codebase/dhtmlx.js" type="text/javascript"></script>
-    <script type="text/javascript">
+<title>Show On Time</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">  
+<link href="../codebase/skyblue/dhtmlx.css" rel="stylesheet" type="text/css"/>
+<script src="../codebase/dhtmlx.js" type="text/javascript"></script>
+<script type="text/javascript">
 
-        /*  Variables de layout */
-        
-        // Celda del dashboard
-        cellDashboard   = "a";
-        tabCell         = "b";
-        
-        // Celdas del dashboard
-        sidebarCell     = "a";
-        
+//Function main dhtmlxEvent
+dhtmlxEvent(window,"load",function(){
+    
+/* INICIALITATION  */    
+   
+    //Static XML
+    tabbarXML       = "Tabbar.xml";       //tabs principales
+    sidebarXML      = "Sidebar.xml";      //Sidebar
+  
+    //Routes
+    icons           = "../resource/icons/"; //rutas de iconos
+    
+    //Cells
+    dashboardCell   = "a";
+    tabbarCell      = "b";
+    sidebarCell     = "a";
+    
+    //setup size
+    siderbarWidth   = 200;
+    
+/* INSTANTIATION  */
+    portalLayout = new dhtmlXLayoutObject(portal,"2U");
 
-        dhtmlxEvent(window,"load",function(){
+    dashBoardLayoutContainer = portalLayout.cells(dashboardCell);
+    dashBoardLayoutContainer.setWidth(siderbarWidth);
 
-/*  INSTANTIATION   */
-            portalLayout = new dhtmlXLayoutObject(portal,"2U");
-            
-            dashBoardLayoutContainer = portalLayout.cells(cellDashboard);
-            dashBoardLayoutContainer.setWidth(200);
-            
-            dashBoardLayout = dashBoardLayoutContainer.attachLayout('2E');
+    dashBoardLayout = dashBoardLayoutContainer.attachLayout('2E');
 
-            /*  Celda izquierda en la que esta el arbol */
-            sidebarContainer = dashBoardLayout.cells(sidebarCell);
-            sidebarContainer.setText("Gestion");
-            sidebarContainer.setWidth(200);
+    /*  Celda izquierda en la que esta el arbol */
+    sidebarContainer = dashBoardLayout.cells(sidebarCell);
+    sidebarContainer.setText("Panel de Control");
+    sidebarContainer.setWidth(siderbarWidth);
 
-            /*  Agrego el sidebar a la celda  */
-            portalSidebar = sidebarContainer.attachSidebar({
-                icons_path:"../resource/icons/"
-            });
+    /*  Agrego el sidebar a la celda  */
+    portalSidebar = sidebarContainer.attachSidebar({
+        icons_path: icons
+    });
 
-            /*  Celda central en la que estaran las pestañas    */  
-            centerContainer = portalLayout.cells(tabCell);
-            centerContainer.hideHeader();
+    /*  Celda central en la que estaran las pestaÃ±as    */  
+    centerContainer = portalLayout.cells(tabbarCell);
+    centerContainer.hideHeader();
 
-            centerTabs  = centerContainer.attachTabbar();
+    centerTabs  = centerContainer.attachTabbar();
 
-/*  END INSTANTIATION   */       
+/* END INSTANTIATION */       
 
-/*  EVENTS  */
-            portalSidebar.attachEvent("onSelect",function(id){
-                centerTabs.tabs(id).setActive();
-            });
+/* EVENTS */
 
-/*  END EVENTS  */     
+    portalSidebar.attachEvent("onSelect",function(id){
+        centerTabs.tabs(id).setActive();
+    });
+    
+/* END EVENTS */     
 
-/*  LOADS   */
-            portalSidebar.loadStruct("Sidebar.xml");
-            centerTabs.loadStruct("Tabs.xml");
+/* LOADS  */
 
-/*  END LOADS   */                
-            });
-    </script>
+    portalSidebar.loadStruct(sidebarXML);
+    centerTabs.loadStruct(tabbarXML);
+    
+/* END LOADS */
+
+/* FUNCTIONS */
+/* END FUNCTIONS */
+
+});//end function main dhtmlxEvent
+
+</script>
 </head>
 <body>
-   <div id="portal"  style="position: fixed; height: 95%; width: 90%;"></div>
+<div id="portal" style="position: fixed; height: 95%; width: 90%;"></div>
 </body>
 </html>

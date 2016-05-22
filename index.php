d79310fe4b3586f95ce9f949861a6fa18b58078c
Hawerd <82hags@gmail.com>
2016-05-22 11:13:43 -0500

    HAG inicio
    inicio de prototipacion

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Show On Time</title>
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=PT+Sans:400,700'>
+        <link href="resources/stilos/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="shortcut icon" type="image/x-icon" href="resources/image/showOnTime48.ico">
+    </head>
+    <body>
+        <div class="page-container">
+            <h1></h1>
+            <IMG src="resources/image/backgrounds/logoSOT.jpg"  width="100" height="100">
+            <form action="" method="post">
+                <input type="text"      name="userName" class="username" placeholder="Nombre de Usuario">
+                <input type="password"  name="password" class="password" placeholder="Contraseña">
+                <button type="submit">Ingresar</button>
+            </form>
+        </div>
+
+    </body>
+</html>

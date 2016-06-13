0f72ced33c31b27d88606cca2152362f46749257
vic759 <vic7592@gmail.com>
2016-06-13 00:15:58 -0500

    Implementación del documento base HTML para Clients-V.php

--- portal/cellsC-Center/clients/Clients-V.php
+++ portal/cellsC-Center/clients/Clients-V.php
@@ -1,8 +1,43 @@
-<?php
+<!DOCTYPE html>
+<!--
+    Name:   Clients-V.php
+    Autor:  Victor Gutierrez
+    Date:   13-Jun-2016
+    Desc:   Modulo encargado de la gestión de clientes
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+***************************************************************************************
 
+    Autor: 
+    Date:
+    Desc:
+    
+-->
+
+<html>
+<head>
+<title>Clientes</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">  
+<link href="../../../codebase/skyblue/dhtmlx.css" rel="stylesheet" type="text/css"/>
+<script src="../../../codebase/dhtmlx.js" type="text/javascript"></script>
+<script type="text/javascript">
+    
+//Function dhtmlxEvent
+dhtmlxEvent(window,"load",function(){
+    //execute dhtmlx init
+    clientsInit();
+});//end function dhtmlxEvent
+
+function clientsInit() {
+    
+/* INITIALIZATION */
+
+/* END INITIALIZATION */
+
+}
+    
+</script>
+</head>
+<body>
+<div id="clientsLayoutDiv" style="position: fixed; height: 99%; width: 99%;"></div>
+</body>
+</html>
